cf1abf0f6e125e5146810d280a18fe72e748ff81
Yoseph Tapano <yosephman76@gmail.com>
2023-02-23 10:39:09 +0300

    0-basic_cache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -10,12 +10,11 @@ class BasicCache(BaseCaching):
     """
     overrides put() and self()
     """
+
     def put(self, Key, item):
-        if key and  item:
+        if key and item:
             self.cache_data[key] = item
 
     def get(self, key):
-            value = self.cache_data.get(key)
-            return value
-
-
+        value = self.cache_data.get(key)
+        return value
