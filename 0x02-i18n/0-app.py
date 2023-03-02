ae1b090fd066ee097575f70464280f36b8ef019c
Yoseph Tapano <yosephman76@gmail.com>
2023-03-02 07:58:32 +0300

    0-app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+file 0-app
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+app.route"(/")
+def index():
+    """desplay the index file"""
+    return render_template("0-index.html")
+
+if __name__== "__main__":
+    app.run(debug=True)
