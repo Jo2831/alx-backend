63834a66802b74af3dfce144c6d998fa528c201a
Yoseph Tapano <yosephman76@gmail.com>
2023-03-02 11:10:45 +0300

    3-app

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -5,9 +5,6 @@ Module 0-app
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
-app = Flask(__name__)
-babel = Babel(app)
-
 
 class Config(object):
     """i18n configuration"""
@@ -16,20 +13,22 @@ class Config(object):
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
+app = Flask(__name__)
 app.config.from_object(Config)
+babel = Babel(app)
 
 
-@app.route("/")
+@app.route("/", strict_slashes=False)
 def index():
     """didplays a basic hello world message"""
-    return render_template("2-index.html")
+    return render_template("3-index.html")
 
 
 @babel.localeselector
 def get_locale():
     """Gets best fmatch locale according to request"""
-    return request.accept_languages.best_match(Config.LANGUAGES)
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port="5000")
